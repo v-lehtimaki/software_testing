ecc2526a2fa4a0f016937166838182cd6604f530
Ville Lehtimäki <ville.lehtimaki@tuni.fi>
2020-11-29 22:34:51 +0200

    Adding a test.

--- test/add.test.js
+++ test/add.test.js
@@ -1,12 +1,14 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
 import add from '../Material/src/add.js';
-const assert = require('chai').assert;
+import chai from 'chai';
 
+var assert = chai.assert;
 
 describe('Adding two integers together with add-function', () => {
     it('Add-function should return 23', () => {
         assert.equal(add(12, 13), 25);
+    });
+    it('Add-function should return 0', () => {
+        assert.equal(add(-1, 1), 0);
     });
 });
 
