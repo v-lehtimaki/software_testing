4864bf8ef5794de3bb300a619a96da3ed049514e
Ville Lehtimäki <ville.lehtimaki@tuni.fi>
2020-12-05 18:25:59 +0200

    Enhancements.

--- tests/words.test.js
+++ tests/words.test.js
@@ -5,18 +5,18 @@ const expect = chai.expect;
 
 describe('Words-function', () => {
     test('Should split the string and return an array of words', () => {
-        expect(words('Today is a cold day')).to.equal([
+        expect(words('Today is a cold day')).to.eql([
             'Today','is','a','cold','day']);
-        expect(words('Today is a cold & sunny day')).to.equal([
+        expect(words('Today is a cold & sunny day')).to.eql([
             'Today','is','a','cold','sunny','day']);
-        expect(words('And now we have 5 days left')).to.equal([
+        expect(words('And now we have 5 days left')).to.eql([
             'And','now','we','have','5','days','left']);
-        expect(words('1 2 34 & / () ?')).to.equal([
+        expect(words('1 2 34 & / () ?')).to.eql([
             '1','2','34']);      
     });
 
     test('Should reject the capitalized letters.', () => {
-        expect(words('TODAY IS A cold DAY'),'[a-z]+'.to.equal(['cold']));
+        expect(words('TODAY IS A cold DAY','[a-z]+').to.eql(['cold']));
     });
 
     test('Should handle invalid parameters without throwing an error', () => {
