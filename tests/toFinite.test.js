078dbbc5c18ae3497dd1cda551d2c203674a03c0
Valtteri Viikari <valtteri.viikari@tuni.fi>
2020-12-07 15:18:18 +0200

    Fix merge conflict

--- tests/toFinite.test.js
+++ tests/toFinite.test.js
@@ -13,24 +13,10 @@ describe('toFinite-function', () => {
         expect(toFinite(Number.MIN_VALUE)).to.equal(Number.MIN_VALUE);
     })
 
-<<<<<<< HEAD
-describe('toFinite-function', () => {
-    test('Should convert the value to a finite number', () => {
-        expect(toInteger(10500)).to.equal(10500);
-        expect(toInteger(3.101)).to.equal(3);
-        expect(toInteger(Number.MIN_VALUE)).to.equal(5e-324);
-        expect(toInteger(Infinity)).to.equal(1.7976931348623157e+308);
-        expect(toInteger(-0.5)).to.equal(-1);
-        expect(toInteger(-4.5)).to.equal(-5);
-        expect(toInteger(-4)).to.equal(-4);
-        expect(toInteger(-0)).to.equal(0);
-    });
-=======
     test("Should convert infinite numbers", () => {
         expect([Infinity, -Infinity]).to.not.include(toFinite(Infinity));
         expect([Infinity, -Infinity]).to.not.include(toFinite(-Infinity));
     })
->>>>>>> 2a522f6fb753e4d53da8ca1e5e2ba653399ec1bb
 
     test('Should handle invalid parameters without throwing', () => {
         expect(() => toFinite('Hello')).to.not.throw();
